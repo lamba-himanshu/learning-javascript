48ee73f10463ca85d419d9df37d504621135ecae
Himanshu Lamba <92923935+lamba-himanshu@users.noreply.github.com>
2024-06-13 13:59:12 +0000

    memory allocation concept

--- basics/datatypeSummery.js
+++ basics/datatypeSummery.js
@@ -26,3 +26,36 @@ let myFunction=function(){
 // console.log(typeof names);
 // console.log(typeof obj);
 // console.log(typeof myFunction);
+
+// =====================memory allocation ======================
+
+// 1. stack memory: used when primitive datatype are defined. 
+//                  when we declare a variable we get the copy of it.
+
+//2. Heap memory: when non primitive datatypes are defined.
+//                when non primitive datatypes like array, objects are defined then heap is created 
+        //and we get the reference(original value) means changes are directly made to original value.
+
+let name1="himanshu";
+let name2=name1;
+//console.log(name2);  //himanshu
+
+name2="sumit";  //we get the copy of name1, changes are not reflected in name1 
+//console.log(name1); //himanshu
+//console.log(name2); //sumit
+
+let user1={              //allocated in heap memory
+    email:"abc@gmail.com",
+    age:18,
+}
+
+let user2=user1; //user2 is referenced to user1 
+//console.log(user1);
+//console.log(user2); //same o/p
+
+user2.email="qwerty@gmail.com";
+console.log(user1); //changes are reflected in user1 that means orginal value also changes in heap memory
+console.log(user2);
+
+
+
