9c695ce344c999531b0eae9040600ba49cb14687
Himanshu Lamba <92923935+lamba-himanshu@users.noreply.github.com>
2024-06-13 07:58:30 +0000

    operations done

--- basics/conversionAndOperation.js
+++ basics/conversionAndOperation.js
@@ -45,5 +45,44 @@ let valueInBoolean = Boolean(isLoggedIn);
 
 let x=33;
 let numberToString = String(x);
-console.log(numberToString);
-console.log(typeof numberToString);
+//console.log(numberToString);
+//console.log(typeof numberToString);
+
+
+// ======================= operations ===============================
+
+let value = 21;
+let negValue = -value;
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(4/2);
+
+
+let str1= "hello";
+let str2 = " world";
+//console.log(str1+str2);
+
+// console.log("1"+2);  //12
+// console.log(1+"2"); //12
+// console.log(1+2+"3"); //33
+// console.log("1"+2+2); //122  when string comes first the other number automatically converted to strings 
+// console.log(2+2+"3"); //43   when number comes first then it performs operation and then append string value
+
+// console.log(true); //true
+// console.log(+true); // 1 because of conversion to number as true is incremented
+//console.log(true+); //error
+// console.log(+"");  //0
+
+//prefix increment
+let a=2;
+let b=++a; //it increments value of and return value after incrementing
+//console.log("a="+a,"b="+b); //3 3
+
+//postfix increment
+let c=3;
+let d=c++; //value in d is 3 and then it increments c 
+console.log("c="+c,"d="+d); //c=4 d=3
