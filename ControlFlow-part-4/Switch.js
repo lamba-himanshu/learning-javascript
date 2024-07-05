71f5601b849e8bde78c956801cfcd3d7b9f55410
Himanshu Lamba <92923935+lamba-himanshu@users.noreply.github.com>
2024-07-05 11:00:05 +0000

    switch statement

---
+++ ControlFlow-part-4/Switch.js
@@ -0,0 +1,30 @@
+//when we have to check a value with multiple cases then switch is used
+//syntax:
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month="april";
+switch (month) {
+    case "january":
+        console.log("january");
+        break;
+    case "february":
+        console.log("february");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case matched");
+        break;
+}
