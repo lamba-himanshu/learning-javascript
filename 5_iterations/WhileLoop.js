e958aab7e6c28ecbf5c1b4ec45d897b9ab230328
Himanshu Lamba <92923935+lamba-himanshu@users.noreply.github.com>
2024-07-06 11:55:45 +0000

    while and do-while loop

---
+++ 5_iterations/WhileLoop.js
@@ -0,0 +1,22 @@
+let i=0;
+while(i<=10){
+    //console.log(`value of i is ${i}`);
+    i=i+2;
+}
+
+const cars = ["honda","suzuki","ford","tata"];
+let index=0;
+while(index < cars.length){
+    //console.log(cars[index]);
+    index++;
+}
+
+
+
+//do-while loop : condition is checked after an iteration and minimum one iteration gets executed
+//let score = 0;
+let score = 11;
+do {
+    console.log(score);
+    score++;
+} while (score<=10);
