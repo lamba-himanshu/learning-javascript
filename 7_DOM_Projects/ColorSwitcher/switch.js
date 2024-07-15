15d5ccd6c8304be4eda21a766b711d11fc8d7ede
Himanshu Lamba <92923935+lamba-himanshu@users.noreply.github.com>
2024-07-15 19:39:53 +0000

    Using Switch Case

--- 7_DOM_Projects/ColorSwitcher/switch.js
+++ 7_DOM_Projects/ColorSwitcher/switch.js
@@ -4,31 +4,62 @@ const h1=document.querySelector('h1');
 const h2=document.querySelector('h2');
 button.forEach(function (b){
     b.addEventListener('click',function(e){
-        if(e.target.id === 'grey'){
-            body.style.backgroundColor = e.target.id;
-            h1.style.color="#fff";
-            h2.style.color="#fff";
+        switch(e.target.id){
+            case "grey":
+                body.style.backgroundColor = e.target.id;
+                h1.style.color="#fff";
+                h2.style.color="#fff";
+                break;
+            
+            case "yellow":
+                body.style.backgroundColor = e.target.id;
+                h1.style.color="#fff";
+                h2.style.color="#fff";
+
+            case "red":
+                body.style.backgroundColor = e.target.id;
+                h1.style.color="#fff";
+                h2.style.color="#fff";
+
+            case "blue":
+                body.style.backgroundColor = e.target.id;
+                h1.style.color="#fff";
+                h2.style.color="#fff";
+            
+            case "purple":
+                body.style.backgroundColor = e.target.id;
+                h1.style.color="#fff";
+                h2.style.color="#fff";
+
+            default:
+                console.log("click on particular color");
+                break;
         }
-        if(e.target.id === 'yellow'){
-            body.style.backgroundColor = e.target.id;
-            h1.style.color="#fff";
-            h2.style.color="#fff";
-        }
-        if(e.target.id === 'red'){
-            body.style.backgroundColor = e.target.id;
-            h1.style.color="#fff";
-            h2.style.color="#fff";
-        }
-        if(e.target.id === 'blue'){
-            body.style.backgroundColor = e.target.id;
-            h1.style.color="#fff";
-            h2.style.color="#fff";
-        }
-        if(e.target.id === 'purple'){
-            body.style.backgroundColor = e.target.id;
-            h1.style.color="#fff";
-            h2.style.color="#fff";
-        }
+        // if(e.target.id === 'grey'){
+        //     body.style.backgroundColor = e.target.id;
+        //     h1.style.color="#fff";
+        //     h2.style.color="#fff";
+        // }
+        // if(e.target.id === 'yellow'){
+        //     body.style.backgroundColor = e.target.id;
+        //     h1.style.color="#fff";
+        //     h2.style.color="#fff";
+        // }
+        // if(e.target.id === 'red'){
+        //     body.style.backgroundColor = e.target.id;
+        //     h1.style.color="#fff";
+        //     h2.style.color="#fff";
+        // }
+        // if(e.target.id === 'blue'){
+        //     body.style.backgroundColor = e.target.id;
+        //     h1.style.color="#fff";
+        //     h2.style.color="#fff";
+        // }
+        // if(e.target.id === 'purple'){
+        //     body.style.backgroundColor = e.target.id;
+        //     h1.style.color="#fff";
+        //     h2.style.color="#fff";
+        // }
         
     });
 });
